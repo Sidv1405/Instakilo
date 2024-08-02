5dd004e9a456a0ca7d2859d1ea122f13f23c0326
Sidv1405 <dangvansi1405@gmail.com>
2024-08-03 06:22:32 +0700

    update UI Login Screen

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -13,53 +13,116 @@ class _LoginScreenState extends State<LoginScreen> {
   FocusNode emailF = FocusNode();
   final password = TextEditingController();
   FocusNode passwordF = FocusNode();
+  bool showPass = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          SizedBox(
-            width: 96.w,
-            height: 100.h,
-          ),
-          Center(
-            child: Image.asset('images/logo.jpg'),
-          ),
-          SizedBox(height: 120.h),
-          textInput(email, Icons.email, "Email", emailF),
-          SizedBox(height: 15.h),
-          textInput(password, Icons.lock, "Password", passwordF),
-          SizedBox(height: 10.h),
-          forgotPassword(),
-          SizedBox(height: 10.h),
-          logIn(),
-          SizedBox(height: 10.h),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.w),
-            child: Container(
-              alignment: Alignment.center,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't have an account?",
-                    style: TextStyle(fontSize: 13.sp),
-                  ),
-                  Text(
-                    " Sign Up",
-                    style: TextStyle(fontSize: 13.sp, color: Colors.blue, fontWeight: FontWeight.bold),
-                  )
-                ],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            SizedBox(
+              width: 96.w,
+              height: 210.h,
+            ),
+            Center(
+              child: Image.asset(
+                'images/logo.jpg',
+                width: 200.w,
+                fit: BoxFit.contain,
               ),
             ),
-          )
+            SizedBox(height: 28.h),
+            textInput(email, "Phone number, username or email", emailF, false),
+            SizedBox(height: 10.h),
+            textInput(password, "Password", passwordF, true),
+            SizedBox(height: 10.h),
+            forgotPassword(),
+            SizedBox(height: 27.h),
+            logIn(),
+            SizedBox(height: 20.h),
+            orUI(),
+            SizedBox(height: 35.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.w),
+              child: Container(
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.facebook_sharp, color: Colors.blue),
+                    Text(
+                      " Log in with Facebook ",
+                      style: TextStyle(fontSize: 13.sp, color: Colors.blue, fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 180.h),
+            signUp(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding orUI() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Divider(
+              thickness: 1,
+              color: Colors.grey,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              'OR',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Divider(
+              thickness: 1,
+              color: Colors.grey,
+            ),
+          ),
         ],
-      )),
+      ),
+    );
+  }
+
+  Widget signUp() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
+      child: Container(
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Don't have an account?",
+              style: TextStyle(fontSize: 13.sp),
+            ),
+            Text(
+              " Sign Up",
+              style: TextStyle(fontSize: 13.sp, color: Colors.blue, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
     );
   }
 
@@ -69,14 +132,14 @@ class _LoginScreenState extends State<LoginScreen> {
       child: Container(
         alignment: Alignment.center,
         width: double.infinity,
-        height: 40,
+        height: 47,
         decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(10.r),
+          color: const Color(0xFF0081FD),
+          borderRadius: BorderRadius.circular(2.r),
         ),
         child: Text(
           "Log In",
-          style: TextStyle(fontSize: 23.sp, color: Colors.white, fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 14.sp, color: Colors.white, fontWeight: FontWeight.bold),
         ),
       ),
     );
@@ -85,30 +148,47 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget forgotPassword() => Padding(
         padding: EdgeInsets.symmetric(horizontal: 10.w),
         child: Text(
-          "Forgot your password?",
+          "Forgot password?",
           style: TextStyle(fontSize: 13.sp, color: Colors.blue, fontWeight: FontWeight.bold),
         ),
       );
 
-  Widget textInput(TextEditingController controller, IconData icon, String hint, FocusNode focusNode) =>
-      Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10.w),
-        child: TextField(
-          style: TextStyle(fontSize: 18.sp, color: Colors.black),
-          controller: controller,
-          focusNode: focusNode,
-          decoration: InputDecoration(
-              hintText: hint,
-              prefixIcon: Icon(icon, color: focusNode.hasFocus ? Colors.black : Colors.grey),
-              contentPadding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 8.h),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5.r),
-                borderSide: BorderSide(color: const Color(0xFF767676), width: 2.w),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5.r),
-                borderSide: BorderSide(color: Colors.black, width: 2.w),
-              )),
+  Widget textInput(TextEditingController controller, String hint, FocusNode focusNode, bool isPassword) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
+      child: TextField(
+        style: TextStyle(fontSize: 16.sp, color: Colors.black),
+        controller: controller,
+        focusNode: focusNode,
+        obscureText: isPassword ? showPass : false,
+        decoration: InputDecoration(
+          fillColor: const Color(0xFFF8F8F8),
+          hintText: hint,
+          hintStyle: TextStyle(fontSize: 14.sp, color: Colors.grey),
+          suffixIcon: isPassword
+              ? IconButton(
+                  icon: Icon(
+                    showPass ? Icons.visibility_off : Icons.visibility,
+                    color: Colors.grey,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      showPass = !showPass;
+                    });
+                  },
+                )
+              : null,
+          contentPadding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 9.h),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5.r),
+            borderSide: BorderSide(color: const Color(0xFF767676), width: 1.w),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5.r),
+            borderSide: BorderSide(color: const Color(0xFF767676), width: 1.w),
+          ),
         ),
-      );
+      ),
+    );
+  }
 }
